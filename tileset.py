2a11104b4e91a3f526a3a6fd21f8289c920d2a0b
Maya <amelie.barbe@ens-lyon.fr>
2021-08-27 09:10:53 +0200

    tileset know if it wraps

--- tileset.py
+++ tileset.py
@@ -5,11 +5,15 @@ class Tileset():
         self.num_tiles = 0
         self.contraints_h = set({}) # Set. (i,j) present iff i can be on top of j.
         self.contraints_v = set({}) # (i,j) present iff i can be at the left of j.
+        self.wrap_horizontal = False
+        self.wrap_vertical = False
         self.tiles = [] # List of images. Implicite tile_num <--> image mapping.
     def process_image(img, tile_size=2,
                       wrap_horizontal=False, wrap_vertical=False):
         """ Takes as input an image, slice it into tiles and compute
             constraints."""
+        self.wrap_horizontal = wrap_horizontal
+        self.wrap_vertical = wrap_vertical
         n,m = img.shape
         shift_h = 0 if wrap_horizontal else tile_size
         shift_v = 0 if wrap_vertical else tile_size
@@ -25,14 +29,14 @@ class Tileset():
                 # Finish extracting tile
                 new_tile = h_slice.take(np.arange(j,j+tile_size), axis=1, mode="wrap")
                 # Check if tile already exists
-                for k,old_tile in enumerate(tiles):
+                for k,old_tile in enumerate(self.tiles):
                     if np.equal(new_tile, old_tile):
                         # Tile already known
                         a[i,j] = k
                         break
                 else:
                     # Tile unknown
-                    tiles.append(new_tile)
+                    self.tiles.append(new_tile)
 
         # Step 2: compute adjacency contraints
         # (Note: this could be made fast with existing numpy functions.)
